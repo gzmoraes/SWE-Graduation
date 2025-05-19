63f00b28925e13adb49fca27b43cb49337b9ce4b
gzmoraes <moraesguh28@gmail.com>
2025-05-18 21:57:40 -0300

    Exercicios de controle txt

--- Exercicios Python/Exercicios de Controle de arquivos/ex1.py
+++ Exercicios Python/Exercicios de Controle de arquivos/ex1.py
@@ -1,3 +1,4 @@
+#Cria um arquivo txt de 0 a 100
 abrirArquivo = open(("crescente.txt"), "w")
 for n in range (1, 101):
     abrirArquivo.write(f"{n};")
--- Exercicios Python/Exercicios de Controle de arquivos/ex2.py
+++ Exercicios Python/Exercicios de Controle de arquivos/ex2.py
@@ -1,3 +1,4 @@
+#Cria um arquivo txt de 100 a 0
 abrirArquivo = open(("decrescente.txt"), "w")
 for n in range (100, 0, -1):
     abrirArquivo.write(f"{n};")
